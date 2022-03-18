e674a1a1ea015a0414177c8e1071b775cdf51812
AuroraMitl <68555321+AuroraMitl@users.noreply.github.com>
2022-03-18 11:27:47 +0400

    Initial commit

---
+++ src/main/webapp/org/firstSpringMVC/HelloController.java
@@ -0,0 +1,14 @@
+package main.webapp.org.firstSpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "helloWorld";
+    }
+
+}
